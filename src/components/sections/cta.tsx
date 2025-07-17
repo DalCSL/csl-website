d128df717f2821a380c80ba9448f5b8b219eed05
GHuyHuynh <huy.huynh@dal.ca>
2025-07-17 20:01:49 -0300

    base

--- src/components/sections/cta.tsx
+++ src/components/sections/cta.tsx
@@ -2,7 +2,7 @@ import GridBackground from "@/components/backgrounds/gird-background";
 
 export default function CTA() {
   return (
-    <GridBackground className="bg-white">
+    <GridBackground className="bg-white min-h-screen flex items-center">
       <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
         <div className="relative isolate overflow-hidden bg-gray-900 px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
           <svg
